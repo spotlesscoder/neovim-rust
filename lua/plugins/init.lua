50d3a66b68b8c8f07c02117c2b1c64ca3faabbd8
spotlesscoder <sunrisechain@gmail.com>
2025-12-28 19:22:31 +0100

    fix: get_install_path is deprecated

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,8 +21,8 @@ return {
     config = function ()
       local mason_registry = require('mason-registry')
       local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
+      local extension_path = vim.fn.expand "$MASON/packages/codelldb/extension/"
+	  local codelldb_path = extension_path .. "adapter/codelldb"
       local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
 	-- If you are on Linux, replace the line above with the line below:
 	-- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
